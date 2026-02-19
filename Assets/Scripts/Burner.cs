7024fe8b317eb9e921d6a976846a02d066980c61
wardtyl <wardtyl@OA-S143-03>
2026-02-19 16:38:10 -0500

    Progress made on rotate button and burner button

--- Assets/Scripts/Burner.cs
+++ Assets/Scripts/Burner.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Burner : MonoBehaviour
 {
+    public GameObject spawningPrefab;
+
+    public float waitDuration;
+    private float waitProgress = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,12 @@ public class Burner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        waitProgress += Time.deltaTime;
+        if (waitProgress > waitDuration)
+        {
+            GameObject spawnedObject = Instantiate(spawningPrefab, transform.position, Quaternion.identity);
+
+            Burner spawnedBurner = spawnedObject.GetComponent<Burner>();
+        }
     }
 }
---
+++ Assets/Scripts/Plate Rotate.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class PlateRotate : MonoBehaviour
+{
+    public float rotationSpeed;
+    public float clockDuration;
+    private float timeWaiting;
+
+    public GameObject spawningPrefab;
+
+    public float waitDuration;
+    private float waitProgress = 0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //sushiSlider.maxValue = clockDuration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // timeWaiting += Time.deltaTime * 1;
+        //sushiSlider.value = timeWaiting;
+
+        //    if (timeWaiting > clockDuration)
+        //    {
+        //        timeWaiting = 0f;
+        //        Debug.Log("Time is up");
+
+        //        //WHEN TIMER IS UP:
+        //        //EITHER:
+        //        //Turn the warper object off when it is on
+        //        //Turn the warper object on when it is off
+        //        bool shouldTurnOn = !warperObject.activeInHierarchy;
+        //        bool shouldTurnOff = warperObject.activeInHierarchy;
+        //        if (shouldTurnOn == true)
+        //        {
+        //            warperObject.SetActive(true);
+        //        }
+
+        //        if (shouldTurnOff)
+        //        {
+        //            warperObject.SetActive(false);
+        //        }
+
+        //    }
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+
+        }
+    }
+}
+
